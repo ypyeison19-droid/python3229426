acf08cbb90792754d811f99c8499b790dfc7c696
Estiben <ypyeison19@gmail.com>
2026-07-30 09:04:11 -0500

    ejemplo4

--- ejemplo4.py
+++ ejemplo4.py
@@ -7,8 +7,17 @@ print(listaVacia)
 listaVacia.append("dato")
 print(listaVacia)
 
-paises = ["Colombia", "Peru", "Ecuador", "Brasil"]
+paises = ["Colombia", "Peru", "Ecuador", "Brasil" , "Venezuela", "Bolivia"]
 
 print(paises)
 print ( "Longitud de la lista es" + str(len(paises)))
 print(f"Longitud de la lista: {len(paises)}")
+
+print(paises[2:4])
+
+paises[1] = "Argentina"
+
+print(paises)
+
+paises.insert(2, "Chile")
+print(paises)
